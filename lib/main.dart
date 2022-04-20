8e76230b70f9c85cf6719c77895577b401d75782
Danielle Kom <daniellekom1@gmail.com>
2022-04-20 12:45:51 -0400

    added more colors

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,23 @@ void main()=> runApp(MaterialApp(
     appBar: AppBar(
       title: const Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
 ), 
 body: const Center(
-  child: Text('hey Ninjas!'),
+  child: Text(
+    'hey Ninjas!',
+    style: TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 2.0,
+      color: Colors.grey,
+    ),
+    ),
 ),
 floatingActionButton: FloatingActionButton(
   onPressed: () {},
   child: const Text('click'),
-  backgroundColor: Colors.green,
+  backgroundColor: Colors.red[600],
 ),
 
 ),
