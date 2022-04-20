e59d789e03b65eb25f18b0114cecacf45e3f6f57
Danielle Kom <daniellekom1@gmail.com>
2022-04-20 14:50:02 -0400

    onpress buttons checking for firing in console

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,38 @@
 
 import 'package:flutter/material.dart';
 
-
-void main()=> runApp(MaterialApp(
-  home: Home(),
-));
-
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: const Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-), 
-body: const Center(
-  child: Image(
-    image: AssetImage('assets/spaceimage3.jpg'),
-    ),
-),
-floatingActionButton: FloatingActionButton(
-  onPressed: () {},
-  child: const Text('click'),
-  backgroundColor: Colors.red[600],
-),
-
-);
+      appBar: AppBar(
+        title: const Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Button'),
+          onPressed: () {
+            // ignore: avoid_print
+            print('you clicked me');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.purple,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              textStyle:
+                  const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('click '),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
